a7d9f6071be3a2fbb90f38138a32396b824ac962
moondev03 <moondev03@gmail.com>
2026-01-15 01:14:04 +0900

    feat: detekt 정적 분석 도구 도입
    * feat: detekt 플러그인 및 설정 추가
    * style: detekt 설정 파일 추가

--- Prezel/build.gradle.kts
+++ Prezel/build.gradle.kts
@@ -3,17 +3,35 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ktlint) apply true
+    alias(libs.plugins.detekt) apply true
 }
 
 subprojects {
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 
     ktlint {
         plugins.withId("com.android.base") {
             android.set(true)
         }
         plugins.withId("org.jetbrains.kotlin.jvm") {
-            android.set(true)
+            android.set(false)
+        }
+    }
+
+    detekt {
+        buildUponDefaultConfig = true
+        config.setFrom(files("$rootDir/detekt-config.yml"))
+        basePath = rootDir.absolutePath
+        parallel = true
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        reports {
+            txt.required.set(true)
+            html.required.set(false)
+            xml.required.set(false)
+            sarif.required.set(false)
         }
     }
 }
