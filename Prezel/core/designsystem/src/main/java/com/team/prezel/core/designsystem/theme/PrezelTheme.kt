b2cb5fe1ff60a42f3cbc25ebb59233b7db0ca8f2
moondev03 <moondev03@gmail.com>
2026-01-21 06:51:57 +0900

    feat: PrezelTheme에 Typography 추가
    * PrezelTheme에 Typography를 사용할 수 있도록 `LocalPrezelTypography` 및 관련 속성을 추가합니다.
    * `PrezelTheme` Composable에서 `PrezelTypographyScheme`을 제공하도록 수정합니다.

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/theme/PrezelTheme.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/theme/PrezelTheme.kt
@@ -6,14 +6,21 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 import com.team.prezel.core.designsystem.foundation.color.PrezelColors
+import com.team.prezel.core.designsystem.foundation.typography.PrezelTypography
 
 private val LocalPrezelColors = staticCompositionLocalOf<PrezelColors> { error("No PrezelColors provided") }
+private val LocalPrezelTypography = staticCompositionLocalOf<PrezelTypography> { error("No PrezelTypography provided") }
 
 object PrezelTheme {
     val colors: PrezelColors
         @Composable
         @ReadOnlyComposable
         get() = LocalPrezelColors.current
+
+    val typography: PrezelTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalPrezelTypography.current
 }
 
 @Composable
@@ -22,9 +29,11 @@ fun PrezelTheme(
     content: @Composable () -> Unit,
 ) {
     val colorScheme = if (isDarkTheme) PrezelColorScheme.Dark else PrezelColorScheme.Light
+    val typographyScheme = PrezelTypographyScheme.Default()
 
     CompositionLocalProvider(
         LocalPrezelColors provides colorScheme,
+        LocalPrezelTypography provides typographyScheme,
         content = content,
     )
 }
