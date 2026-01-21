cea673959dbcdc0f9150a72e652284139f50db59
Ham BeomJoon <hbj0802@naver.com>
2026-01-21 21:21:24 +0900

    feat: PrezelIcons 아이콘 리소스 정의 추가
    디자인시스템 모듈에 커스텀 아이콘 리소스 ID를 관리하는 `PrezelIcons` 객체를 추가했습니다.
    
    * ArrowLeft, Calendar, Check, Home, Profile 등 주요 아이콘 리소스 매핑 추가

---
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/icon/PrezelIcons.kt
@@ -0,0 +1,49 @@
+package com.team.prezel.core.designsystem.icon
+
+import com.team.prezel.core.designsystem.R
+
+// Prezel icons. Material icons are [ImageVector]s, custom icons are drawable resource IDs.
+object PrezelIcons {
+    val ArrowLeft = R.drawable.ic_arrow_left
+    val Balloon = R.drawable.ic_balloon
+    val Blank = R.drawable.ic_blank
+    val Calendar = R.drawable.ic_calendar
+    val Cancel = R.drawable.ic_cancel
+    val CancelCircleFilled = R.drawable.ic_cancel_circle_filled
+    val CancelCircleOutlined = R.drawable.ic_cancel_circle_outlined
+    val Check = R.drawable.ic_check
+    val CheckCircleFilled = R.drawable.ic_check_circle_filled
+    val CheckCircleOutlined = R.drawable.ic_check_circle_outlined
+    val ChevronDown = R.drawable.ic_chevron_down
+    val ChevronLeft = R.drawable.ic_chevron_left
+    val ChevronRight = R.drawable.ic_chevron_right
+    val ChevronUp = R.drawable.ic_chevron_up
+    val College = R.drawable.ic_college
+    val Company = R.drawable.ic_company
+    val Folder = R.drawable.ic_folder
+    val Gallery = R.drawable.ic_gallery
+    val Hand = R.drawable.ic_hand
+    val Home = R.drawable.ic_home
+    val Info = R.drawable.ic_info
+    val InfoCircle = R.drawable.ic_info_circle
+    val Kakao = R.drawable.ic_kakao
+    val Mic = R.drawable.ic_mic
+    val Pause = R.drawable.ic_pause
+    val Person = R.drawable.ic_person
+    val Play = R.drawable.ic_play
+    val Plus = R.drawable.ic_plus
+    val Profile = R.drawable.ic_profile
+    val RadioCircleFilled = R.drawable.ic_radio_circle_filled
+    val RadioCircleOutlined = R.drawable.ic_radio_circle_outlined
+    val Recording = R.drawable.ic_recording
+    val Reset = R.drawable.ic_reset
+    val Rotate = R.drawable.ic_rotate
+    val Stop = R.drawable.ic_stop
+    val Trophy = R.drawable.ic_trophy
+    val Video = R.drawable.ic_video
+    val Voice = R.drawable.ic_voice
+    val Warning = R.drawable.ic_warning
+    val WarningCircle = R.drawable.ic_warning_circle
+    val ZoomIn = R.drawable.ic_zoom_in
+    val ZoomOut = R.drawable.ic_zoom_out
+}
