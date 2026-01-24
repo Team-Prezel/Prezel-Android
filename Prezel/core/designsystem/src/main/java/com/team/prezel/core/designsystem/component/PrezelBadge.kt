5026ca228c61223d76b81fc07bce219a80c38348
Ham BeomJoon <hbj0802@naver.com>
2026-01-24 20:39:33 +0900

    chore: ktlint 적용

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/PrezelBadge.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/PrezelBadge.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -81,7 +79,8 @@ private fun PrezelNumberBadge(
             .background(
                 color = backgroundColor,
                 shape = PrezelTheme.shapes.V1000,
-            ).padding(horizontal = PrezelTheme.spacing.V4, vertical = PrezelTheme.spacing.V2),
+            )
+            .padding(horizontal = PrezelTheme.spacing.V4, vertical = PrezelTheme.spacing.V2),
     ) {
         Text(
             text = count.toString(),
