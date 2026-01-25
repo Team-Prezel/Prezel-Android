7e8b71825ab228033647ba92e671fb361b8b33e8
moondev03 <moondev03@gmail.com>
2026-01-26 04:36:18 +0900

    refactor: `PrezelRadio` 컴포넌트 내부 패딩 로직 수정
    `PrezelRadio` 컴포넌트의 가독성과 명확성을 개선하기 위해 `Spacer`와 패딩 관련 코드를 수정했습니다.
    
    *   `Row`의 `toggleable` Modifier 체이닝에서 `padding`을 다음 줄로 이동하여 가독성을 높였습니다.
    *   수평 간격을 명시적으로 나타내기 위해 `Spacer(modifier = Modifier.padding(V4))`를 `Spacer(modifier = Modifier.width(V4))`로 변경했습니다.

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/PrezelRadio.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/PrezelRadio.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -114,7 +115,7 @@ fun PrezelRadio(
             modifier = Modifier.clearAndSetSemantics { },
         )
 
-        Spacer(modifier = Modifier.padding(PrezelTheme.spacing.V4))
+        Spacer(modifier = Modifier.width(PrezelTheme.spacing.V4))
 
         Text(
             text = text,
