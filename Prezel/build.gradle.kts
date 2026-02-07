f099ab30b800f9d7e3381d541f0983927376b5c2
Ham BeomJoon <hbj0802@naver.com>
2026-02-07 22:01:53 +0900

    chore: 불필요한 android-library 플러그인 제거
    `libs.versions.toml`과 `build.gradle.kts`에서 더 이상 사용되지 않는 `com.android.library` 플러그인 종속성을 제거합니다.

--- Prezel/build.gradle.kts
+++ Prezel/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.detekt) apply true
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.android.library) apply false
 }
 
 subprojects {
