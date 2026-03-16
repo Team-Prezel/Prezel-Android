9c71ef8e10740b7f5bc73f4cc7d4d9efa1e048b0
Ham BeomJoon <hbj0802@naver.com>
2026-03-16 17:15:06 +0900

    feat: 중복 클릭 방지를 위한 `ComposeMultipleEventCutter` 유틸리티 추가
    중복 이벤트 발생을 방지하기 위해 일정 시간 내의 연속된 클릭을 제한하는 `ComposeMultipleEventCutter` 객체와 확장 함수를 추가했습니다.
    
    *   `clickOnce`: 파라미터 유무에 따른 클릭 이벤트 중복 방지 확장 함수 추가
    *   `ComposeMultipleEventCutter`: 마지막 이벤트 발생 시간을 기준으로 이벤트 실행 여부를 판단하는 내부 객체 구현

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/util/ComposeMultipleEventCutter.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/util/ComposeMultipleEventCutter.kt
@@ -1,10 +1,14 @@
 package com.team.prezel.core.designsystem.util
 
-fun (() -> Unit).clickOnce(time: Long = 200L): () -> Unit = {
-    ComposeMultipleEventCutter.processEvent(time) { this() }
-}
+fun (() -> Unit).clickOnce(time: Long = 200L): () -> Unit =
+    {
+        ComposeMultipleEventCutter.processEvent(time) { this() }
+    }
 
-fun <T> ((T) -> Unit).clickOnce(value: T, time: Long = 200L) {
+fun <T> ((T) -> Unit).clickOnce(
+    value: T,
+    time: Long = 200L,
+) {
     ComposeMultipleEventCutter.processEvent(time) { this(value) }
 }
 
@@ -14,7 +18,10 @@ internal object ComposeMultipleEventCutter {
 
     private var lastEventTimeMs: Long = 0
 
-    fun processEvent(time: Long, event: () -> Unit) {
+    fun processEvent(
+        time: Long,
+        event: () -> Unit,
+    ) {
         if (now - lastEventTimeMs >= time) {
             event.invoke()
         }
