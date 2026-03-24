706de2aeca5a34e27b57dd775db9a76453293f59
moondev03 <moondev03@gmail.com>
2026-03-25 05:02:35 +0900

    style: BasicPreview 배경색 리소스 수정
    `BasicPreview` 어노테이션의 `backgroundColor` 값을 올바른 8자리 16진수 색상 코드(`0xFFFFFFFF`)로 수정하였습니다.

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/preview/BasicPreview.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/preview/BasicPreview.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
     showBackground = true,
-    backgroundColor = 0xFFFFFF,
+    backgroundColor = 0xFFFFFFFF,
     uiMode = Configuration.UI_MODE_NIGHT_NO,
 )
 annotation class BasicPreview
