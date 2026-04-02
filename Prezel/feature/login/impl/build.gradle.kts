25a1afad8fa11981e0e8e89f7ffbffd031ec844c
moondev03 <moondev03@gmail.com>
2026-04-02 23:35:46 +0900

    feat: 로그인 모듈 내 BuildConfig 활성화 및 약관 URL 필드 추가

--- Prezel/feature/login/impl/build.gradle.kts
+++ Prezel/feature/login/impl/build.gradle.kts
@@ -1,9 +1,20 @@
+import com.team.prezel.buildlogic.convention.external.localProperty
+
 plugins {
     alias(libs.plugins.prezel.android.feature.impl)
 }
 
 android {
     namespace = "com.team.prezel.feature.login.impl"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField("String", "PRIVACY_POLICY_URL", "\"${localProperty("privacy.policy.url").get()}\"")
+        buildConfigField("String", "TERMS_OF_SERVICE_URL", "\"${localProperty("terms.of.service.url").get()}\"")
+    }
 }
 
 dependencies {
