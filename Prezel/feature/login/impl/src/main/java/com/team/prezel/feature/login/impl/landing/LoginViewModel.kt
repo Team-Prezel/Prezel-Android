a809fbb83a8805fec05751381ea8b4d0474810f8
moondev03 <moondev03@gmail.com>
2026-04-02 23:57:45 +0900

    feat: LoginViewModel 내 디버그 모드 전용 로직 추가

--- Prezel/feature/login/impl/src/main/java/com/team/prezel/feature/login/impl/landing/LoginViewModel.kt
+++ Prezel/feature/login/impl/src/main/java/com/team/prezel/feature/login/impl/landing/LoginViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.team.prezel.core.auth.model.AuthProvider
 import com.team.prezel.core.auth.model.AuthResult
+import com.team.prezel.feature.login.impl.BuildConfig
 import com.team.prezel.feature.login.impl.landing.contract.LoginUiEffect
 import com.team.prezel.feature.login.impl.landing.contract.LoginUiIntent
 import com.team.prezel.feature.login.impl.landing.contract.LoginUiState
@@ -20,52 +21,57 @@ import javax.inject.Inject
 
 @HiltViewModel
 internal class LoginViewModel
-    @Inject
-    constructor() : ViewModel() {
-        private val _uiState = MutableStateFlow(LoginUiState())
-        val uiState: StateFlow<LoginUiState> = _uiState
-        private val currentState: LoginUiState
-            get() = uiState.value
+@Inject
+constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState: StateFlow<LoginUiState> = _uiState
+    private val currentState: LoginUiState
+        get() = uiState.value
 
-        private val _uiEffect = Channel<LoginUiEffect>()
-        val uiEffect: Flow<LoginUiEffect> = _uiEffect.receiveAsFlow()
+    private val _uiEffect = Channel<LoginUiEffect>()
+    val uiEffect: Flow<LoginUiEffect> = _uiEffect.receiveAsFlow()
 
-        fun onIntent(intent: LoginUiIntent) {
-            when (intent) {
-                is LoginUiIntent.OnClickLogin -> handleClickLogin(provider = intent.provider)
-                is LoginUiIntent.OnLoginResult -> handleLoginResult(result = intent.result)
-            }
+    fun onIntent(intent: LoginUiIntent) {
+        when (intent) {
+            is LoginUiIntent.OnClickLogin -> handleClickLogin(provider = intent.provider)
+            is LoginUiIntent.OnLoginResult -> handleLoginResult(result = intent.result)
         }
-
-        private fun update(reducer: LoginUiState.() -> LoginUiState) {
-            _uiState.update(reducer)
-        }
-
-        private fun handleClickLogin(provider: AuthProvider) {
-            if (currentState.isLoading) return
-
-            viewModelScope.launch {
-                update { copy(isLoading = true) }
-//                _uiEffect.send(LoginUiEffect.LaunchLogin(provider = provider))
-                _uiEffect.send(LoginUiEffect.NavigateToTerms)
-            }
-        }
-
-        private fun handleLoginResult(result: AuthResult) {
-            viewModelScope.launch {
-                update { copy(isLoading = false) }
-
-                when (result) {
-                    AuthResult.Success -> _uiEffect.send(LoginUiEffect.NavigateToTerms)
-                    AuthResult.Cancelled -> _uiEffect.send(LoginUiEffect.ShowMessage(LoginUiMessage.LoginCancelled))
-                    is AuthResult.Failure -> _uiEffect.send(LoginUiEffect.ShowMessage(result.toUiMessage()))
-                }
-            }
-        }
-
-        private fun AuthResult.Failure.toUiMessage(): LoginUiMessage =
-            when (this) {
-                AuthResult.Failure.RateLimited -> LoginUiMessage.LoginFailedRateLimited
-                AuthResult.Failure.Unknown -> LoginUiMessage.LoginFailedUnknown
-            }
     }
+
+    private fun update(reducer: LoginUiState.() -> LoginUiState) {
+        _uiState.update(reducer)
+    }
+
+    private fun handleClickLogin(provider: AuthProvider) {
+        if (currentState.isLoading) return
+
+        viewModelScope.launch {
+            update { copy(isLoading = true) }
+
+            // todo: MVP 개발 완료 후 해당 조건 제거
+            if (BuildConfig.DEBUG) {
+                _uiEffect.send(LoginUiEffect.NavigateToTerms)
+            } else {
+                _uiEffect.send(LoginUiEffect.LaunchLogin(provider = provider))
+            }
+        }
+    }
+
+    private fun handleLoginResult(result: AuthResult) {
+        viewModelScope.launch {
+            update { copy(isLoading = false) }
+
+            when (result) {
+                AuthResult.Success -> _uiEffect.send(LoginUiEffect.NavigateToTerms)
+                AuthResult.Cancelled -> _uiEffect.send(LoginUiEffect.ShowMessage(LoginUiMessage.LoginCancelled))
+                is AuthResult.Failure -> _uiEffect.send(LoginUiEffect.ShowMessage(result.toUiMessage()))
+            }
+        }
+    }
+
+    private fun AuthResult.Failure.toUiMessage(): LoginUiMessage =
+        when (this) {
+            AuthResult.Failure.RateLimited -> LoginUiMessage.LoginFailedRateLimited
+            AuthResult.Failure.Unknown -> LoginUiMessage.LoginFailedUnknown
+        }
+}
