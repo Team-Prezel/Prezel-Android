fb91880f31765f0237b4c79ba3fc9dfe6984354e
moondev03 <moondev03@gmail.com>
2026-04-16 22:46:26 +0900

    chore: `UserRepositoryImpl` 내 TODO 주석의 대소문자 수정
    `checkNicknameDuplication` 메서드 내의 `TODO` 주석을 `todo`로 변경하였습니다.

--- Prezel/core/data/src/main/java/com/team/prezel/core/data/repository/UserRepositoryImpl.kt
+++ Prezel/core/data/src/main/java/com/team/prezel/core/data/repository/UserRepositoryImpl.kt
@@ -25,7 +25,7 @@ internal class UserRepositoryImpl @Inject constructor() : UserRepository {
         }
 
     override suspend fun checkNicknameDuplication(nickname: Nickname): Result<Boolean> {
-        // TODO: 실제 API 연동 후 서버 중복 검사 결과를 반환하도록 교체
+        // todo: 실제 API 연동 후 서버 중복 검사 결과를 반환하도록 교체
         delay(200)
         return Result.success(false)
     }
