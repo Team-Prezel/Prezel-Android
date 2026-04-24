1f7138397f96f42bc7a763a4792448d0981517bb
moondev03 <moondev03@gmail.com>
2026-04-25 01:05:17 +0900

    refactor: PrezelNavigationBar 스캐폴드 내 AnimatedVisibility 종료 애니메이션 수정
    `PrezelNavigationBarScaffold`에서 내비게이션 바가 사라질 때의 애니메이션을 명시적으로 설정하였습니다.
    
    * `AnimatedVisibility`의 `exit` transition을 `ExitTransition.None`으로 설정하여 바텀바가 사라질 때의 전환 효과를 제거하였습니다.

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/PrezelNavigationBar.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/PrezelNavigationBar.kt
@@ -2,6 +2,7 @@ package com.team.prezel.core.designsystem.component
 
 import androidx.annotation.DrawableRes
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -106,7 +107,10 @@ fun PrezelNavigationScaffold(
     Scaffold(
         modifier = modifier,
         bottomBar = {
-            AnimatedVisibility(visible = showNavigationBar) {
+            AnimatedVisibility(
+                visible = showNavigationBar,
+                exit = ExitTransition.None,
+            ) {
                 PrezelNavigationBar {
                     PrezelNavigationScope(this).navigationItems()
                 }
