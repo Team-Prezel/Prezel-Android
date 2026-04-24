2875f362ae45832d36544828d1d7f8ca216a000a
MunJangHun <105299421+moondev03@users.noreply.github.com>
2026-04-24 20:58:31 +0900

    PrezelProgressBar 구현 (#107)
    * refactor: 디자인 시스템 컴포넌트 패키지 구조 변경 (modal -> feedback)
    
    `PrezelDialog` 및 `PrezelSnackbar` 관련 컴포넌트의 위치를 `modal` 패키지에서 `feedback` 패키지로 이동하고 관련 임포트 경로를 수정하였습니다.
    
    * **refactor: Dialog 및 Snackbar 컴포넌트 패키지 이동**
        * `component.modal.dialog` -> `component.feedback.dialog`
        * `component.modal.snackbar` -> `component.feedback.snackbar`
        * 이동된 파일: `PrezelDialog.kt`, `PrezelDialogScope.kt`, `PrezelSnackbar.kt`, `SnackbarHost.kt`
    
    * **refactor: 패키지 변경에 따른 의존성 업데이트**
        * `PrezelNavigationBar`, `ProfileScreen`, `HistoryScreen`, `HomeScreen`, `LoginScreen`, `DoubleBackToExitHandler` 등에서 변경된 패키지 경로로 `import` 문 수정
    
    * feat: PrezelProgressBar 구현
    
    * feat: 커스텀 ProgressBar 컴포넌트 추가
    
    디자인 시스템에 작업 진행 상태를 시각적으로 나타내는 `PrezelProgressBar` 컴포넌트를 추가했습니다.
    
    *   `progress`: 0.0에서 1.0 사이의 값을 받아 진행률 표시 (범위 외 값은 `coerceIn`으로 보정)
    *   `progressColor`, `trackColor`: 진행 바와 배경 트랙의 색상을 커스텀할 수 있도록 파라미터 제공
    *   디자인 시스템의 `PrezelTheme.shapes.V1000`을 적용하여 둥근 모서리 스타일 구현
    *   `ProgressBarRangeInfo`를 통한 접근성(Semantics) 지원
    *   컴포넌트 확인을 위한 `PrezelProgressBarPreview` 추가

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/feedback/dialog/PrezelDialog.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/feedback/dialog/PrezelDialog.kt
@@ -1,4 +1,4 @@
-package com.team.prezel.core.designsystem.component.modal.dialog
+package com.team.prezel.core.designsystem.component.feedback.dialog
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/feedback/snackbar/SnackbarHost.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/component/feedback/snackbar/SnackbarHost.kt
@@ -1,4 +1,4 @@
-package com.team.prezel.core.designsystem.component.modal.snackbar
+package com.team.prezel.core.designsystem.component.feedback.snackbar
 
 import androidx.annotation.DrawableRes
 import androidx.compose.material3.SnackbarDuration
