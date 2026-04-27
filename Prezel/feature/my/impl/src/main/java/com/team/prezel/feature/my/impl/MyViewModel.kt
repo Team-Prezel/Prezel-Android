a4b2ca2e75fb994d015647b04cb33f76708ed228
moondev03 <moondev03@gmail.com>
2026-04-28 03:37:40 +0900

    refactor: MyViewModel 내 미사용 의존성 및 import 제거
    * **refactor: `MyViewModel` 생성자 및 의존성 정리**
        * 현재 사용되지 않는 `LogoutUseCase` 및 `WithdrawUseCase` 의존성을 생성자에서 제거했습니다.
        * 의존성 제거에 따라 불필요해진 관련 import 구문을 삭제했습니다.

--- Prezel/feature/my/impl/src/main/java/com/team/prezel/feature/my/impl/MyViewModel.kt
+++ Prezel/feature/my/impl/src/main/java/com/team/prezel/feature/my/impl/MyViewModel.kt
@@ -1,7 +1,5 @@
 package com.team.prezel.feature.my.impl
 
-import com.team.prezel.core.domain.usecase.auth.LogoutUseCase
-import com.team.prezel.core.domain.usecase.auth.WithdrawUseCase
 import com.team.prezel.core.ui.base.BaseViewModel
 import com.team.prezel.feature.my.impl.contract.MyUiEffect
 import com.team.prezel.feature.my.impl.contract.MyUiIntent
@@ -10,10 +8,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-internal class MyViewModel @Inject constructor(
-    private val logoutUseCase: LogoutUseCase,
-    private val withdrawUseCase: WithdrawUseCase,
-) : BaseViewModel<MyUiState, MyUiIntent, MyUiEffect>(MyUiState()) {
+internal class MyViewModel @Inject constructor() : BaseViewModel<MyUiState, MyUiIntent, MyUiEffect>(MyUiState()) {
     override fun onIntent(intent: MyUiIntent) {
         // todo: 프로필 화면 구현 작업에서 진행
     }
