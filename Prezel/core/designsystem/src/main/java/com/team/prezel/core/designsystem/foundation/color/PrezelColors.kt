8facb7682083f99f48cd7b88a4e2f0e70ded55a5
moondev03 <moondev03@gmail.com>
2026-05-07 05:55:32 +0900

    refactor: 디자인 시스템 컬러 토큰 수정

--- Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/foundation/color/PrezelColors.kt
+++ Prezel/core/designsystem/src/main/java/com/team/prezel/core/designsystem/foundation/color/PrezelColors.kt
@@ -15,7 +15,6 @@ data class PrezelColors(
     val bgRegular: Color,
     val bgMedium: Color,
     val bgLarge: Color,
-    val bgScrim: Color,
     val bgDisabled: Color,
     // Text
     // 정보와 콘텐츠를 명확하게 전달하기 위해 사용하는 색상입니다.
@@ -57,4 +56,8 @@ data class PrezelColors(
     // 흰색과 검정색을 제공하여 시각적 대비, 보조, 구분 등에 활용되는 절대값 색상입니다.
     val solidWhite: Color,
     val solidBlack: Color,
+    // Component
+    // 컴포넌트를 위한 색상입니다.
+    val chipContainerSecondaryFilledDefault: Color,
+    val scrimContainer: Color,
 )
